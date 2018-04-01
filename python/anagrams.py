2086b96a925cdcbe35af66993f3ca1b23dcd2656
iBlackEyes <570963025@qq.com>
2018-04-01 17:49:05 +0800

    my first code in github

---
+++ python/anagrams.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# qinliang@meituan.com
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        s = sorted(s)
+        t = sorted(t)
+        if s == t:
+            return True
+        else:
+            return False
